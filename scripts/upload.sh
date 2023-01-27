8d9245ef51ae824f31b1520e95de7cfcba8d143b
Nico1704 <b1704nc@gmail.com>
2023-01-27 22:03:35 +0100

    .

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -40,7 +40,7 @@ cd out/target/product/${Device}
 transfer wet ${Files} > link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
 
 # Mirror to oshi.at
-curl -T $[Files] https://oshi.at/${FILENAME}/${TIMEOUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; }
+curl -T ${Files} https://oshi.at/${FILENAME}/${TIMEOUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; }
 
 DL_LINK=$(cat link.txt | grep Download | cut -d\  -f3)
 MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1)
