3cebce6a55994e72218420c51f5fd58c2cf43d8a
Nico1704 <b1704nc@gmail.com>
2023-01-29 11:49:09 +0100

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -26,9 +26,9 @@ telegram_message() {
 # Variables
 REPO="https://github.com/PitchBlackRecoveryProject/manifest_pb"                                         #<--- Change the wanted Repo here (Link to SHRP, PBRP or TWRP Manifest Link)
 Branch="android-12.1"                                                                                  #<--- Set Source Branch here (SHRP-12.1, android-12.1 for PBRP or twrp-12.1)
-Device="z3s"                                                                                         #<--- Set Device Codename here
+Device="c2s"                                                                                         #<--- Set Device Codename here
 OEM="samsung"                                                                                        #<--- Set Device Manufactor here
-DeviceTree="https://github.com/Nico170420/android_device_samsung_z3s.git"                            #<--- Put Device Tree Link here
+DeviceTree="https://github.com/Nico170420/android_device_samsung_c2s.git"                            #<--- Put Device Tree Link here
 DTBranch="pbrp"                                                                                     #<--- Set the DT Branch Name here
 
 # Initialize Repo Manifest (SHRP/TWRP/PBRP)
